c47651301a95b4cbbb3fcbcab6014794811537ce
Hung <hunghx@rikkeisoft.com>
2023-03-13 14:31:07 +0700

    clean-code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TennisGame.getScore("Hung","Nam",3,2));
+        System.out.println(TennisGame.getScore("Hung","Nam",10,9));
+        System.out.println(TennisGame.getScore("Hung","Nam",8,8));
+        System.out.println(TennisGame.getScore("Hung","Nam",2,2));
+        System.out.println(TennisGame.getScore("Hung","Nam",4,2));
+    }
+}
--- src/TennisGameTest.java
+++ src/TennisGameTest.java
@@ -1,6 +1,8 @@
 
 
-import org.junit.Test;
+
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
